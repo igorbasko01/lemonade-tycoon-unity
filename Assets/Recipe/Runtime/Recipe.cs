e8cc6212822fcf6cae07158aac0aad5b3337e63d
igorbasko01 <igorbasko@gmail.com>
2024-06-07 18:15:11 +0300

    convert Recipe to scriptable object

--- Assets/Recipe/Runtime/Recipe.cs
+++ Assets/Recipe/Runtime/Recipe.cs
@@ -1,21 +1,39 @@
 using System;
 using System.Collections.Generic;
 using baskorp.IngredientsCatalog.Runtime;
+using UnityEngine;
 
-namespace baskorp.Recipes.Runtime {
-    public class Recipe {
-        public string Name { get; }
-        public List<QuantifiableIngredient> Ingredients { get; }
+namespace baskorp.Recipes.Runtime
+{
+    [CreateAssetMenu(fileName = "New Recipe", menuName = "Recipe")]
+    public class Recipe : ScriptableObject
+    {
+        [SerializeField]
+        private string recipeName;
+        [SerializeField]
+        private List<QuantifiableIngredient> ingredients;
+        public string Name { get => recipeName; }
+        public List<QuantifiableIngredient> Ingredients { get => ingredients; }
 
-        public Recipe(string name, List<QuantifiableIngredient> ingredients) {
-            if (ingredients == null) {
+        private void OnValidate()
+        {
+            if (ingredients == null)
+            {
                 throw new ArgumentException("A recipe must have at least one ingredient");
             }
-            if (ingredients.Count == 0) {
+            if (ingredients.Count == 0)
+            {
                 throw new ArgumentException("A recipe must have at least one ingredient");
             }
-            Name = name;
-            Ingredients = ingredients;
+        }
+
+        public static Recipe Create(string name, List<QuantifiableIngredient> ingredients)
+        {
+            var recipe = CreateInstance<Recipe>();
+            recipe.recipeName = name;
+            recipe.ingredients = ingredients;
+            recipe.OnValidate();
+            return recipe;
         }
     }
 }
--- Assets/Recipe/Tests/RecipeTests.cs
+++ Assets/Recipe/Tests/RecipeTests.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using baskorp.Recipes.Runtime;
 using baskorp.IngredientsCatalog.Runtime;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace baskorp.Recipes.Tests
 {
@@ -12,7 +11,7 @@ namespace baskorp.Recipes.Tests
         [Test]
         public void RecipeInitializedWithANameAndAListOfIngredients()
         {
-            var recipe = new Recipe("Test Recipe", new List<QuantifiableIngredient> { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 5f)});
+            var recipe = Recipe.Create("Test Recipe", new List<QuantifiableIngredient> { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 5f)});
             Assert.AreEqual("Test Recipe", recipe.Name);
             Assert.AreEqual(1, recipe.Ingredients.Count);
         }
@@ -20,25 +19,25 @@ namespace baskorp.Recipes.Tests
         [Test]
         public void RecipeInitializedWithANameAndAnEmptyListOfIngredients_ThrowsArgumentException()
         {
-            Assert.Throws<System.ArgumentException>(() => new Recipe("Test Recipe", new List<QuantifiableIngredient>()));
+            Assert.Throws<System.ArgumentException>(() => Recipe.Create("Test Recipe", new List<QuantifiableIngredient>()));
         }
 
         [Test]
         public void RecipeInitializedWithANameAndNullListOfIngredients_ThrowsArgumentException()
         {
-            Assert.Throws<System.ArgumentException>(() => new Recipe("Test Recipe", null));
+            Assert.Throws<System.ArgumentException>(() => Recipe.Create("Test Recipe", null));
         }
 
         [Test]
         [Ignore("Need to implement")]
-        public void RecipeInitializedWithANameAndAListOfIngredients_IngredientsAreTheSame()
+        public void RecipeInitializedWithANameAndAListOfIngredients_IngredientsAreTheSame_ThrowArgumentException()
         {
             
         }
 
         [Test]
         [Ignore("Need to implement")]
-        public void RecipeInitializedWithANameAndAListOfIngredients_IngredientsAreDifferent()
+        public void RecipeInitializedWithANameAndAListOfIngredients_IngredientsAreDifferent_Success()
         {
             
         }
