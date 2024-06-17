d27f7aaecf1a4e8e4a608fa7838e0025ae0144d3
igorbasko01 <igorbasko@gmail.com>
2024-06-17 21:45:49 +0300

    add base Calendar folder structure

---
+++ Assets/Calendar/Tests/CalendarTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace baskorp.Calendar.Tests
+{
+    [TestFixture]
+    public class CalendarTests
+    {
+        [Test]
+        [Ignore("Not implemented yet")]
+        public void Create_Calendar_Success()
+        {
+            Assert.Pass();
+        }
+    }
+}
