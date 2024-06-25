c14690dffb7b641b60d68d1e9efb3dfe3563466e
igorbasko01 <igorbasko@gmail.com>
2024-06-25 21:51:49 +0300

    add skytype and temperature type probability calculation

--- Assets/Weather/Runtime/WeatherForecaster.cs
+++ Assets/Weather/Runtime/WeatherForecaster.cs
@@ -1,15 +1,45 @@
 using System;
+using System.Collections.Generic;
 using baskorp.Calendars.Runtime;
+using UnityEngine;
 
 namespace baskorp.Weather.Runtime {
     public class WeatherForecaster {
+        private Dictionary<Season, Dictionary<SkyType, double>> skyTypeProbabilities = new() {
+            {Season.Spring, new Dictionary<SkyType, double> {{SkyType.Clear, 0.7}, {SkyType.Rainy, 0.3}}},
+            {Season.Summer, new Dictionary<SkyType, double> {{SkyType.Clear, 0.9}, {SkyType.Rainy, 0.1}}},
+            {Season.Autumn, new Dictionary<SkyType, double> {{SkyType.Clear, 0.6}, {SkyType.Rainy, 0.4}}},
+            {Season.Winter, new Dictionary<SkyType, double> {{SkyType.Clear, 0.3}, {SkyType.Rainy, 0.7}}}
+        };
+
+        private Dictionary<Season, Dictionary<TemperatureType, double>> temperatureTypeProbabilities = new() {
+            {Season.Spring, new Dictionary<TemperatureType, double> {{TemperatureType.Hot, 0.1}, {TemperatureType.Cold, 0.1}, {TemperatureType.Mild, 0.8}}},
+            {Season.Summer, new Dictionary<TemperatureType, double> {{TemperatureType.Hot, 0.8}, {TemperatureType.Cold, 0.1}, {TemperatureType.Mild, 0.1}}},
+            {Season.Autumn, new Dictionary<TemperatureType, double> {{TemperatureType.Hot, 0.1}, {TemperatureType.Cold, 0.8}, {TemperatureType.Mild, 0.1}}},
+            {Season.Winter, new Dictionary<TemperatureType, double> {{TemperatureType.Hot, 0.1}, {TemperatureType.Cold, 0.8}, {TemperatureType.Mild, 0.1}}}
+        };
+
         public WeatherForecast GetForecast(Date date) {
-            var random = new Random(date.GetHashCode());
+            var random = new System.Random(date.GetHashCode());
             var skyTypes = Enum.GetValues(typeof(SkyType));
-            var randomSkyType = (SkyType) skyTypes.GetValue(random.Next(skyTypes.Length));
+            var skyTypeProbabilitiesForSeason = skyTypeProbabilities[date.Season];
+            var randomSkyType = GetRandomWeatherType(random, skyTypes, skyTypeProbabilitiesForSeason);
+            var temperatureTypeProbabilitiesForSeason = temperatureTypeProbabilities[date.Season];
             var temperatureTypes = Enum.GetValues(typeof(TemperatureType));
-            var randomTemperatureType = (TemperatureType) temperatureTypes.GetValue(random.Next(temperatureTypes.Length));
+            var randomTemperatureType = GetRandomWeatherType(random, temperatureTypes, temperatureTypeProbabilitiesForSeason);
             return new WeatherForecast(randomSkyType, randomTemperatureType);
+        }
+
+        private T GetRandomWeatherType<T>(System.Random random, Array weatherTypes, Dictionary<T, double> probabilities) {
+            var randomValue = random.NextDouble();
+            var cumulativeProbability = 0.0;
+            foreach (var weatherType in weatherTypes) {
+                cumulativeProbability += probabilities[(T) weatherType];
+                if (randomValue < cumulativeProbability) {
+                    return (T) weatherType;
+                }
+            }
+            return (T) weatherTypes.GetValue(weatherTypes.Length - 1);
         }
     }
 }
--- Assets/Weather/Tests/WeatherTests.cs
+++ Assets/Weather/Tests/WeatherTests.cs
@@ -20,5 +20,319 @@ namespace baskorp.Weather.Tests {
             var forecast2 = forecaster.GetForecast(new Date(2, 1, 2020));
             Assert.AreNotEqual(forecast1, forecast2);
         }
+        [Test]
+        public void WeatherForecastForDifferentYearReturnsDifferentForecast() {
+            var forecaster = new WeatherForecaster();
+            var forecast1 = forecaster.GetForecast(new Date(1, 1, 2020));
+            var forecast2 = forecaster.GetForecast(new Date(1, 1, 2021));
+            Assert.AreNotEqual(forecast1, forecast2);
+        }
+        [Test]
+        public void WeatherForecastForDifferentMonthReturnsDifferentForecast() {
+            var forecaster = new WeatherForecaster();
+            var forecast1 = forecaster.GetForecast(new Date(1, 1, 2020));
+            var forecast2 = forecaster.GetForecast(new Date(1, 2, 2020));
+            Assert.AreNotEqual(forecast1, forecast2);
+        }
+        [Test]
+        public void WeatherForecastSummerClearDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int clearDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var summer = new Date(1, 6, i);
+                var forecast = forecaster.GetForecast(summer);
+                if (forecast.SkyType == SkyType.Clear) {
+                    clearDays++;
+                }
+            }
+            float clearDayProbability = (float) clearDays / numOfIterations;
+            Assert.That(clearDayProbability, Is.InRange(0.89f, 0.91f));
+        }
+        [Test]
+        public void WeatherForecastSpringClearDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int clearDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var spring = new Date(1, 3, i);
+                var forecast = forecaster.GetForecast(spring);
+                if (forecast.SkyType == SkyType.Clear) {
+                    clearDays++;
+                }
+            }
+            float clearDayProbability = (float) clearDays / numOfIterations;
+            Assert.That(clearDayProbability, Is.InRange(0.69f, 0.71f));
+        }
+        [Test]
+        public void WeatherForecastAutumnClearDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int clearDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var autumn = new Date(1, 9, i);
+                var forecast = forecaster.GetForecast(autumn);
+                if (forecast.SkyType == SkyType.Clear) {
+                    clearDays++;
+                }
+            }
+            float clearDayProbability = (float) clearDays / numOfIterations;
+            Assert.That(clearDayProbability, Is.InRange(0.59f, 0.61f));
+        }
+        [Test]
+        public void WeatherForecastWinterClearDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int clearDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var winter = new Date(1, 12, i);
+                var forecast = forecaster.GetForecast(winter);
+                if (forecast.SkyType == SkyType.Clear) {
+                    clearDays++;
+                }
+            }
+            float clearDayProbability = (float) clearDays / numOfIterations;
+            Assert.That(clearDayProbability, Is.InRange(0.29f, 0.31f));
+        }
+        [Test]
+        public void WeatherForecastSummerRainyDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int rainyDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var summer = new Date(1, 6, i);
+                var forecast = forecaster.GetForecast(summer);
+                if (forecast.SkyType == SkyType.Rainy) {
+                    rainyDays++;
+                }
+            }
+            float rainyDayProbability = (float) rainyDays / numOfIterations;
+            Assert.That(rainyDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastSpringRainyDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int rainyDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var spring = new Date(1, 3, i);
+                var forecast = forecaster.GetForecast(spring);
+                if (forecast.SkyType == SkyType.Rainy) {
+                    rainyDays++;
+                }
+            }
+            float rainyDayProbability = (float) rainyDays / numOfIterations;
+            Assert.That(rainyDayProbability, Is.InRange(0.29f, 0.31f));
+        }
+        [Test]
+        public void WeatherForecastAutumnRainyDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int rainyDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var autumn = new Date(1, 9, i);
+                var forecast = forecaster.GetForecast(autumn);
+                if (forecast.SkyType == SkyType.Rainy) {
+                    rainyDays++;
+                }
+            }
+            float rainyDayProbability = (float) rainyDays / numOfIterations;
+            Assert.That(rainyDayProbability, Is.InRange(0.39f, 0.41f));
+        }
+        [Test]
+        public void WeatherForecastWinterRainyDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int rainyDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var winter = new Date(1, 12, i);
+                var forecast = forecaster.GetForecast(winter);
+                if (forecast.SkyType == SkyType.Rainy) {
+                    rainyDays++;
+                }
+            }
+            float rainyDayProbability = (float) rainyDays / numOfIterations;
+            Assert.That(rainyDayProbability, Is.InRange(0.69f, 0.71f));
+        }
+        [Test]
+        public void WeatherForecastSummerHotDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int hotDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var summer = new Date(1, 6, i);
+                var forecast = forecaster.GetForecast(summer);
+                if (forecast.TemperatureType == TemperatureType.Hot) {
+                    hotDays++;
+                }
+            }
+            float hotDayProbability = (float) hotDays / numOfIterations;
+            Assert.That(hotDayProbability, Is.InRange(0.79f, 0.81f));
+        }
+        [Test]
+        public void WeatherForecastSpringHotDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int hotDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var spring = new Date(1, 3, i);
+                var forecast = forecaster.GetForecast(spring);
+                if (forecast.TemperatureType == TemperatureType.Hot) {
+                    hotDays++;
+                }
+            }
+            float hotDayProbability = (float) hotDays / numOfIterations;
+            Assert.That(hotDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastAutumnHotDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int hotDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var autumn = new Date(1, 9, i);
+                var forecast = forecaster.GetForecast(autumn);
+                if (forecast.TemperatureType == TemperatureType.Hot) {
+                    hotDays++;
+                }
+            }
+            float hotDayProbability = (float) hotDays / numOfIterations;
+            Assert.That(hotDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastWinterHotDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int hotDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var winter = new Date(1, 12, i);
+                var forecast = forecaster.GetForecast(winter);
+                if (forecast.TemperatureType == TemperatureType.Hot) {
+                    hotDays++;
+                }
+            }
+            float hotDayProbability = (float) hotDays / numOfIterations;
+            Assert.That(hotDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastSummerColdDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int coldDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var summer = new Date(1, 6, i);
+                var forecast = forecaster.GetForecast(summer);
+                if (forecast.TemperatureType == TemperatureType.Cold) {
+                    coldDays++;
+                }
+            }
+            float coldDayProbability = (float) coldDays / numOfIterations;
+            Assert.That(coldDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastSpringColdDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int coldDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var spring = new Date(1, 3, i);
+                var forecast = forecaster.GetForecast(spring);
+                if (forecast.TemperatureType == TemperatureType.Cold) {
+                    coldDays++;
+                }
+            }
+            float coldDayProbability = (float) coldDays / numOfIterations;
+            Assert.That(coldDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastAutumnColdDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int coldDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var autumn = new Date(1, 9, i);
+                var forecast = forecaster.GetForecast(autumn);
+                if (forecast.TemperatureType == TemperatureType.Cold) {
+                    coldDays++;
+                }
+            }
+            float coldDayProbability = (float) coldDays / numOfIterations;
+            Assert.That(coldDayProbability, Is.InRange(0.79f, 0.81f));
+        }
+        [Test]
+        public void WeatherForecastWinterColdDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int coldDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var winter = new Date(1, 12, i);
+                var forecast = forecaster.GetForecast(winter);
+                if (forecast.TemperatureType == TemperatureType.Cold) {
+                    coldDays++;
+                }
+            }
+            float coldDayProbability = (float) coldDays / numOfIterations;
+            Assert.That(coldDayProbability, Is.InRange(0.79f, 0.81f));
+        }
+        [Test]
+        public void WeatherForecastSummerMildDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int mildDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var summer = new Date(1, 6, i);
+                var forecast = forecaster.GetForecast(summer);
+                if (forecast.TemperatureType == TemperatureType.Mild) {
+                    mildDays++;
+                }
+            }
+            float mildDayProbability = (float) mildDays / numOfIterations;
+            Assert.That(mildDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastSpringMildDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int mildDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var spring = new Date(1, 3, i);
+                var forecast = forecaster.GetForecast(spring);
+                if (forecast.TemperatureType == TemperatureType.Mild) {
+                    mildDays++;
+                }
+            }
+            float mildDayProbability = (float) mildDays / numOfIterations;
+            Assert.That(mildDayProbability, Is.InRange(0.79f, 0.81f));
+        }
+        [Test]
+        public void WeatherForecastAutumnMildDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int mildDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var autumn = new Date(1, 9, i);
+                var forecast = forecaster.GetForecast(autumn);
+                if (forecast.TemperatureType == TemperatureType.Mild) {
+                    mildDays++;
+                }
+            }
+            float mildDayProbability = (float) mildDays / numOfIterations;
+            Assert.That(mildDayProbability, Is.InRange(0.09f, 0.11f));
+        }
+        [Test]
+        public void WeatherForecastWinterMildDayProbability() {
+            int numOfIterations = 1000;
+            var forecaster = new WeatherForecaster();
+            int mildDays = 0;
+            for (int i = 0; i < numOfIterations; i++) {
+                var winter = new Date(1, 12, i);
+                var forecast = forecaster.GetForecast(winter);
+                if (forecast.TemperatureType == TemperatureType.Mild) {
+                    mildDays++;
+                }
+            }
+            float mildDayProbability = (float) mildDays / numOfIterations;
+            Assert.That(mildDayProbability, Is.InRange(0.09f, 0.11f));
+        }
     }
 }
