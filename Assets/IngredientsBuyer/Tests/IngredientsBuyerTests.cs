f2cc5325f2b831db5ed02229dcccd2152b1481b3
igorbasko01 <igorbasko@gmail.com>
2024-06-28 12:06:36 +0300

    IngredientsBuyer handles not enough money or invalid ingredient

--- Assets/IngredientsBuyer/Tests/IngredientsBuyerTests.cs
+++ Assets/IngredientsBuyer/Tests/IngredientsBuyerTests.cs
@@ -34,5 +34,35 @@ namespace baskorp.IngredientsBuyers.Tests
             Assert.AreEqual(50f, wallet.Balance);
             Assert.AreEqual("Lemon", inventory.Ingredients[0].Metadata.Name);
         }
+
+        [Test]
+        public void IngredientsBuyer_Buy_NotEnoughMoney()
+        {
+            var buyer = new IngredientsBuyer();
+            var inventory = new IngredientsInventoryManager();
+            var catalog = new IngredientsCatalogManager(_catalogIngredients);
+            var wallet = new Wallet(1);
+            var ingredientToBuy = QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 5f);
+
+            var result = buyer.Buy(ingredientToBuy, inventory, catalog, wallet);
+            Assert.AreEqual(PurchaseResultType.NotEnoughMoney, result.ResultType);
+            Assert.AreEqual(0, inventory.Ingredients.Count);
+            Assert.AreEqual(1f, wallet.Balance);
+        }
+
+        [Test]
+        public void IngredientsBuyer_Buy_IngredientNotFound()
+        {
+            var buyer = new IngredientsBuyer();
+            var inventory = new IngredientsInventoryManager();
+            var catalog = new IngredientsCatalogManager(_catalogIngredients);
+            var wallet = new Wallet(100);
+            var ingredientToBuy = QuantifiableIngredient.Create(IngredientMetadata.Create("New Ingredient"), 5f);
+
+            var result = buyer.Buy(ingredientToBuy, inventory, catalog, wallet);
+            Assert.AreEqual(PurchaseResultType.IngredientNotFound, result.ResultType);
+            Assert.AreEqual(0, inventory.Ingredients.Count);
+            Assert.AreEqual(100f, wallet.Balance);
+        }
     }
 }
