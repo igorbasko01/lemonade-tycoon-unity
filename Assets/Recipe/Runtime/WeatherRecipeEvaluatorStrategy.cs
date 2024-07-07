03baea2241a3ffc52f98e5461f31a783639ab56a
igorbasko01 <igorbasko@gmail.com>
2024-07-07 16:56:07 +0300

    Finalize WeatherRecipeEvaluatorStrategy

--- Assets/Recipe/Runtime/WeatherRecipeEvaluatorStrategy.cs
+++ Assets/Recipe/Runtime/WeatherRecipeEvaluatorStrategy.cs
@@ -1,20 +1,48 @@
 using baskorp.Calendars.Runtime;
 using baskorp.Weather.Runtime;
 
-namespace baskorp.Recipes.Runtime {
-    public class WeatherRecipeEvaluatorStrategy : IRecipeEvaluatorStrategy {
+namespace baskorp.Recipes.Runtime
+{
+    public class WeatherRecipeEvaluatorStrategy : IRecipeEvaluatorStrategy
+    {
 
         private IWeatherForecaster forecaster;
 
-        public WeatherRecipeEvaluatorStrategy(IWeatherForecaster forecaster) {
+        public WeatherRecipeEvaluatorStrategy(IWeatherForecaster forecaster)
+        {
             this.forecaster = forecaster;
         }
-        public float Evaluate(Recipe recipe, Date date) {
+        public float Evaluate(Recipe recipe, Date date)
+        {
             var forecast = forecaster.GetForecast(date);
-            if (forecast.SkyType == SkyType.Clear && forecast.TemperatureType == TemperatureType.Hot) {
+            if (forecast.SkyType == SkyType.Clear && forecast.TemperatureType == TemperatureType.Hot)
+            {
                 return 1f;
             }
-            return 0f;
+            else if (forecast.SkyType == SkyType.Clear && forecast.TemperatureType == TemperatureType.Mild)
+            {
+                return 0.8f;
+            }
+            else if (forecast.SkyType == SkyType.Clear && forecast.TemperatureType == TemperatureType.Cold)
+            {
+                return 0.5f;
+            }
+            else if (forecast.SkyType == SkyType.Rainy && forecast.TemperatureType == TemperatureType.Hot)
+            {
+                return 0.7f;
+            }
+            else if (forecast.SkyType == SkyType.Rainy && forecast.TemperatureType == TemperatureType.Mild) 
+            { 
+                return 0.4f;
+            }
+            else if (forecast.SkyType == SkyType.Rainy && forecast.TemperatureType == TemperatureType.Cold)
+            {
+                return 0.2f;
+            }
+            else
+            {
+                return 0f;
+            }
         }
     }
 }
--- Assets/Recipe/Tests/RecipeEvaluatorStrategyTests.cs
+++ Assets/Recipe/Tests/RecipeEvaluatorStrategyTests.cs
@@ -38,5 +38,55 @@ namespace baskorp.Recipes.Tests
             var result = strategy.Evaluate(recipe, new Calendar(1, 3, 2021).CurrentDate);
             Assert.AreEqual(1f, result);
         }
+
+        [Test]
+        public void WeatherRecipeEvaluatorStrategy_Returns80pctOnClearAndNormal() {
+            var mockForecaster = new Mock<IWeatherForecaster>();
+            mockForecaster.Setup(f => f.GetForecast(It.IsAny<Date>())).Returns(new WeatherForecast(SkyType.Clear, TemperatureType.Mild));
+            var strategy = new WeatherRecipeEvaluatorStrategy(mockForecaster.Object);
+            var recipe = Recipe.Create("Lemonade", new List<QuantifiableIngredient>() { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 1) });
+            var result = strategy.Evaluate(recipe, new Calendar(1, 3, 2021).CurrentDate);
+            Assert.AreEqual(0.8f, result);
+        }
+
+        [Test]
+        public void WeatherRecipeEvaluatorStrategy_Returns50pctOnClearAndCold() {
+            var mockForecaster = new Mock<IWeatherForecaster>();
+            mockForecaster.Setup(f => f.GetForecast(It.IsAny<Date>())).Returns(new WeatherForecast(SkyType.Clear, TemperatureType.Cold));
+            var strategy = new WeatherRecipeEvaluatorStrategy(mockForecaster.Object);
+            var recipe = Recipe.Create("Lemonade", new List<QuantifiableIngredient>() { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 1) });
+            var result = strategy.Evaluate(recipe, new Calendar(1, 3, 2021).CurrentDate);
+            Assert.AreEqual(0.5f, result);
+        }
+
+        [Test]
+        public void WeatherRecipeEvaluatorStrategy_Returns70pctOnRainyAndHot() {
+            var mockForecaster = new Mock<IWeatherForecaster>();
+            mockForecaster.Setup(f => f.GetForecast(It.IsAny<Date>())).Returns(new WeatherForecast(SkyType.Rainy, TemperatureType.Hot));
+            var strategy = new WeatherRecipeEvaluatorStrategy(mockForecaster.Object);
+            var recipe = Recipe.Create("Lemonade", new List<QuantifiableIngredient>() { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 1) });
+            var result = strategy.Evaluate(recipe, new Calendar(1, 3, 2021).CurrentDate);
+            Assert.AreEqual(0.7f, result);
+        }
+
+        [Test]
+        public void WeatherRecipeEvaluatorStrategy_Returns40pctOnRainyAndMild() {
+            var mockForecaster = new Mock<IWeatherForecaster>();
+            mockForecaster.Setup(f => f.GetForecast(It.IsAny<Date>())).Returns(new WeatherForecast(SkyType.Rainy, TemperatureType.Mild));
+            var strategy = new WeatherRecipeEvaluatorStrategy(mockForecaster.Object);
+            var recipe = Recipe.Create("Lemonade", new List<QuantifiableIngredient>() { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 1) });
+            var result = strategy.Evaluate(recipe, new Calendar(1, 3, 2021).CurrentDate);
+            Assert.AreEqual(0.4f, result);
+        }
+
+        [Test]
+        public void WeatherRecipeEvaluatorStrategy_Returns20pctRainyAndCold() {
+            var mockForecaster = new Mock<IWeatherForecaster>();
+            mockForecaster.Setup(f => f.GetForecast(It.IsAny<Date>())).Returns(new WeatherForecast(SkyType.Rainy, TemperatureType.Cold));
+            var strategy = new WeatherRecipeEvaluatorStrategy(mockForecaster.Object);
+            var recipe = Recipe.Create("Lemonade", new List<QuantifiableIngredient>() { QuantifiableIngredient.Create(IngredientMetadata.Create("Lemon"), 1) });
+            var result = strategy.Evaluate(recipe, new Calendar(1, 3, 2021).CurrentDate);
+            Assert.AreEqual(0.2f, result);
+        }
     }
 }
